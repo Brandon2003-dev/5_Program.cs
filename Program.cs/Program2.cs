5aa8c8cf29f7c4c21436f45251de614c85fe1666
Brandon Emerson <111790942+Brandon2003-dev@users.noreply.github.com>
2023-08-18 20:02:04 -0600

    Add files via upload

---
+++ Program.cs/Program2.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ArregloEjercicio2;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] nombreCompleto = { "Brandon", "Emerson" };
+
+        foreach (string valor in nombreCompleto)
+        {
+            Console.WriteLine(valor);
+        }
+
+        int longitudArreglo = nombreCompleto.Length;
+
+        Console.WriteLine("Tamaño del arreglo: " + longitudArreglo);
+        foreach (string valor in nombreCompleto)
+        {
+            Console.WriteLine("Valor: " + valor);
+        }
+    }
+}
---
+++ Program.cs/Program3.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Ejercicio3;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+
+        string[] array = { "Perro", "Casa", "Cebra", "Tortuga", "Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol" };
+
+        Console.WriteLine("posiciones inicializadas:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Posición {i}: {array[i]}");
+        }
+
+        Array.Sort(array);
+
+        Console.WriteLine("\n ordenado:");
+        foreach (string elemento in array)
+        {
+            Console.WriteLine(elemento);
+        }
+    }
+}
